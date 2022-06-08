6bddb1ddcfea3035a9b630d6dc513a00aac4421a
Austin Williams <guitarmusashi@gmail.com>
2022-06-08 15:36:03 +0000

    Added timer that ticks down

--- pick.py
+++ pick.py
@@ -1,13 +1,39 @@
 import json
 import random
-# 1, 2, 3 - easy, medium, hard
-leetcodes = json.load(open("leetcodes.json"))
+import time
+import sys
+import datetime
 
-# easy only
-leetcodes = [x for x in leetcodes if x[1] == "Easy"]
 
-for i in range(4):
+
+def pickRandom(leetcodes, numChoices):
+  print()
+  for i in range(numChoices):
     choice = random.choice(leetcodes)
+    leetcodes.remove(choice)
     print(f"{i}) {choice[0]} ({choice[1]}) {choice[2]}")
+  print()
 
-print("\nTime Remaining: 1 hr 10 min")
+
+def timer(remaining):
+  for remaining in range(remaining, -1, -1):
+      sys.stdout.write("\r")
+      sys.stdout.write(f"Time Remaining: {datetime.timedelta(seconds=remaining)} ") 
+      sys.stdout.flush()
+      time.sleep(1)
+  print("\rTIMES UP!!!                   ")
+
+
+def main():
+  # 1, 2, 3 - easy, medium, hard
+  leetcodes = json.load(open("leetcodes.json"))
+  
+  # easy only
+  leetcodes = [x for x in leetcodes if x[1] == "Easy"]
+
+  pickRandom(leetcodes, 4)
+
+  # 1 hr 10 min
+  timer(5)  
+
+main()
