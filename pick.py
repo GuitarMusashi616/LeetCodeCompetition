62e4347d8617c9ae0f9c284ee235cd2fc64024fa
Austin Williams <guitarmusashi@gmail.com>
2022-06-08 16:28:03 +0000

    reorganized into class

--- pick.py
+++ pick.py
@@ -4,40 +4,47 @@ import time
 import sys
 import datetime
 
+class LeetCodePicker:
+  def __init__(self, filename):
+    self.leetcodes = self.load_leetcodes(filename)
+    self.not_picked = {x[0] for x in self.leetcodes}
+    self.easy_leetcodes = self.filter_leetcodes_by_difficulty("easy")
+    self.i = 1
+    print()
+  
+  def load_leetcodes(self, filename):
+    return json.load(open(filename))
 
-def pickRandom(leetcodes, numChoices, not_picked):
-  for i in range(numChoices):
-    choice = random.choice(leetcodes)
-    while choice[0] not in not_picked:
+  def filter_leetcodes_by_difficulty(self, difficulty):
+    return [x for x in self.leetcodes if x[1].lower() == difficulty.lower()]
+
+  def pickRandom(self, numChoices, difficulty=None):
+    leetcodes = self.leetcodes if difficulty is None else self.filter_leetcodes_by_difficulty(difficulty)
+    for _ in range(numChoices):
       choice = random.choice(leetcodes)
-      
-    not_picked.remove(choice[0])
-    print(f"{i}) {choice[0]} ({choice[1]})\t- {choice[2]}")
+      while choice[0] not in self.not_picked:
+        choice = random.choice(leetcodes)
+        
+      self.not_picked.remove(choice[0])
+      print(f"{self.i}) {choice[0]} ({choice[1]})\t- {choice[2]}")
+      self.i += 1
 
-
-def timer(remaining):
-  for remaining in range(remaining, -1, -1):
-      sys.stdout.write("\r")
-      sys.stdout.write(f"Time Remaining: {datetime.timedelta(seconds=remaining)} ") 
-      sys.stdout.flush()
-      time.sleep(1)
-  print("\rTIMES UP!!!                   ")
+  def timer(self, remaining):
+    print()
+    for remaining in range(remaining, -1, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write(f"Time Remaining: {datetime.timedelta(seconds=remaining)} ") 
+        sys.stdout.flush()
+        time.sleep(1)
+    print("\rTIMES UP!!!                   ")
 
 
 def main():
-  # 1, 2, 3 - easy, medium, hard
-  leetcodes = json.load(open("leetcodes.json"))
-  not_picked = {x[0] for x in leetcodes}
-  
-  # easy only
-  easy = [x for x in leetcodes if x[1] == "Easy"]
-  
-  print()
-  pickRandom(easy, 2, not_picked)
-  pickRandom(leetcodes, 2, not_picked)
-  print()
-  
+  lcp = LeetCodePicker("leetcodes.json")
+  lcp.pickRandom(2, "easy")
+  lcp.pickRandom(2)
+
   # 1 hr 10 min
-  timer(1*60**2 + 10*60)  
+  lcp.timer(1*60**2 + 10*60)
 
 main()
